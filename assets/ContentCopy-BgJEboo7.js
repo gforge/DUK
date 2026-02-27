8ff608d4b7bf68ca68822f6d04ec6cc814eb07f4
Max Gordon <max@gforge.se>
2026-02-27 14:13:25 +0100

    Updates

--- assets/ContentCopy-BgJEboo7.js
+++
@@ -1,1 +0,0 @@
-import{S as e,c as o,j as t}from"./index-BKb_bhC-.js";const s=Object.freeze(Object.defineProperty({__proto__:null,SEED_STATE:e},Symbol.toStringTag,{value:"Module"})),n=o(t.jsx("path",{d:"M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2m0 16H8V7h11z"}));export{n as C,s};
