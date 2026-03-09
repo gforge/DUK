f9adeaa190c87581461e1dc787b65fd947d054f0
Max Gordon <max@gforge.se>
2026-03-09 23:33:48 +0100

    Updates

--- assets/ContentCopy-BA37YeYs.js
+++
@@ -1,1 +0,0 @@
-import{by as e,c as o,j as t,S as c}from"./index-DNJat9La.js";const a=Object.freeze(Object.defineProperty({__proto__:null,SEED_STATE:c,buildMinimalSeed:e},Symbol.toStringTag,{value:"Module"})),n=o(t.jsx("path",{d:"M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2m0 16H8V7h11z"}));export{n as C,a as s};
---
+++ assets/addDays-DdXdPvqI.js
@@ -0,0 +1,1 @@
+import{t as s,c as i}from"./cases-o9wxnVG1.js";function c(a,r,e){const t=s(a,e?.in);return isNaN(r)?i(e?.in||a,NaN):(r&&t.setDate(t.getDate()+r),t)}export{c as a};
